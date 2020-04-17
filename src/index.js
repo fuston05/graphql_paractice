e46d3138dd20b5bdc9f54ee00034bcd1581c9425
fuston05 <fuston05@yahoo.com>
2020-04-16 20:34:14 -0400

    adds basic types, resolvers and mutations

--- src/index.js
+++ src/index.js
@@ -1,21 +1,72 @@
+require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga');
+
+// fake database
+const users= [];
+let count= 1;
 
 //typeDefs SDL
 const typeDefs = `
-  type= Query {
-    info: String!
+  type Query {
+    users: [User!]!
+    user(id: ID!) : User
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    phone: Phone!
+  }
+
+  type Phone {
+    number: String!
+    belongsTo: User!
+  }
+
+  type Mutation {
+    createUser(name: String!) : User
   }
 `;// end typeDefs
 
 //resolvers (object)
 const resolvers = {
   Query: {
-    info: () => "My test info string for graphQL."
-  }
+    user: (parent, args, context, info) => {
+      return users.find( user => parseInt(user.id) === parseInt(args.id))
+    },//user
+
+    users: () => {return users}
+  },//end query
+
+  Mutation: {
+    createUser: (parent, args, context, info) => {  
+      const newUser= {name: args.name, id: count};
+      if(users.push(newUser)){
+        count++;
+        return newUser;
+      }//end if
+    },//end createUser
+
+  }//end Mutation
 }//end resolvers
+
+
+
+
+//server options
+const opts= {
+  port: process.env.PORT,
+  endpoint: '/graphql'
+}
 
 //server
 const server = new GraphQLServer({
   typeDefs,
-  resolvers
+  resolvers,
+  opts
 });//end server
+
+//server start
+server.start(() =>{
+  console.log(`Server running on http://localhost:${opts.port}${opts.endpoint}`);
+});
